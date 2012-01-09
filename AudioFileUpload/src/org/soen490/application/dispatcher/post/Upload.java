ee3c0550e35afc9b9033f0807d06edad4f23f062
Sotirios Delimanolis <sotodel_89@hotmail.com>
2012-01-09 12:31:28 -0500

    it was missing finally, adam

--- AudioFileUpload/src/org/soen490/application/dispatcher/post/Upload.java
+++ AudioFileUpload/src/org/soen490/application/dispatcher/post/Upload.java
@@ -11,10 +11,11 @@ import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.soen490.application.dispatcher.Dispatcher;
+import org.soen490.domain.command.UploadCommand;
 
 public class Upload extends Dispatcher {
 
-	private static final String PATH = "C:\\Users\\Soto\\Desktop\\";
+
 	private static final String TEMP_DIRECTORY = "C:\\Users\\Soto\\Desktop\\Temp\\";
 	private static final long REQUEST_MAX_SIZE = 50 * 1024;
 	
@@ -36,34 +37,12 @@ public class Upload extends Dispatcher {
 				
 				//HashMap<String, String> params = new HashMap<String, String>();
 				
-				
 				upload.setSizeMax(REQUEST_MAX_SIZE);
-
-				List items = upload.parseRequest(request);
-				Iterator it = items.iterator();
 				
-				while (it.hasNext()) {
-					FileItem item = (FileItem) it.next();
-					
-					if(item.isFormField()) {
-						
-					} else {
-						String contentType = item.getContentType();
-						String fileName = item.getName();
-						String fieldName = item.getFieldName();
-						boolean isInMemory = item.isInMemory();
-						long sizeInBytes = item.getSize();
-						
-						File uploadedFile = new File(PATH + "new_audio1.amr");
-						
-						item.write(uploadedFile); 
-						System.out.println("Content type: " + contentType);
-						System.out.println("Field: " + fieldName);
-						System.out.println("File name: " + fileName);
-						System.out.println("Size: " + sizeInBytes);
-						System.out.println("Is in memory:" + isInMemory);
-					}
-				}
+				request.setAttribute("items", upload.parseRequest(request));
+
+				UploadCommand uc = new UploadCommand();
+				uc.execute(helper);
 	
 			} catch (Exception ex) {
 				throw new ServletException(ex);
