75e0844e630573b3e11f2d899e92c0908abb0cbf
Ben Crudo <ben.crudo@gmail.com>
2012-02-16 04:15:10 -0500

    Added a test for the userProxxy. Will add more but I got the ball rolling

--- MasterServer/src/tests/UserProxyTest.java
+++ MasterServer/src/tests/UserProxyTest.java
@@ -15,15 +15,33 @@
 
 package tests;
 
-import org.junit.Test;
+import java.io.IOException;
 
-public class UserProxyTest {
+import domain.user.User;
+import domain.user.UserFactory;
+import domain.user.UserProxy;
+import domain.user.UserType;
+import junit.framework.TestCase;
+
+
+public class UserProxyTest extends TestCase {
+	
+	private final long uid = 3425635465657L;
+	private final String email = "example@example.com";
+	private final String password = "password";
+	private final UserType userType = UserType.USER_NORMAL;
+	private final int version = 1;
 	
 	
-	
-	@Test
-	public void testFunctionality()
-	{
+	public void testConstructorAndGetUid() throws IOException {
+		// First we create an object via the factory so it get's sent to the IdentityMap as well
+		User realUser = UserFactory.createClean(uid, email, password, userType, version);
 		
+		// Create a proxy for the real object
+		UserProxy userProxy = new UserProxy(uid);
+		
+		// Get the ID from the proxy and match it to the realUsers
+		assertEquals(userProxy.getUid(), realUser.getUid());
 	}
+	
 }
