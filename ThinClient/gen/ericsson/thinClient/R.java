fe99b45a2bc22e2d6794839b0fa7b98ddbc4a8c0
Adam Harrison <adamdharrison@gmail.com>
2012-03-12 03:36:42 -0400

    Ignoring large folders so I don't have to piecewise commit.

--- ThinClient/gen/ericsson/thinClient/R.java
+++ ThinClient/gen/ericsson/thinClient/R.java
@@ -11,13 +11,24 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int downvote=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int next=0x7f020002;
+        public static final int pause=0x7f020003;
+        public static final int play=0x7f020004;
+        public static final int prev=0x7f020005;
+        public static final int record=0x7f020006;
+        public static final int square_button=0x7f020007;
+        public static final int stoprecord=0x7f020008;
+        public static final int upvote=0x7f020009;
     }
     public static final class id {
-        public static final int action=0x7f050001;
-        public static final int next=0x7f050002;
-        public static final int prev=0x7f050000;
-        public static final int record=0x7f050003;
+        public static final int action=0x7f050003;
+        public static final int downvote=0x7f050001;
+        public static final int next=0x7f050004;
+        public static final int prev=0x7f050002;
+        public static final int record=0x7f050005;
+        public static final int upvote=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
