7b41bde5effc30d5964741ab5408ba4b7c2acbdd
Adam Harrison <adamdharrison@gmail.com>
2012-03-13 02:11:20 -0400

    Using apache graceful restart, we acheieve a very quick restart of the server, and lose no requests. Max delay is under a second.

--- LoadBalancer/setup.sh
+++ LoadBalancer/setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Shell script to install the balancer from a base debian distribution.
 
@@ -6,16 +6,19 @@ CFG_DIR=/etc/apache2
 RUN_SCRIPT=/etc/init.d/apache2
 MOD_ENA=$CFG_DIR/mods-enabled
 MOD_AVA=$CFG_DIR/mods-available
+MAIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+UPDATE_SCRIPT=$MAIN_DIR/updater.pl
 
 # Base update.
 sudo apt-get update
 # Install apache and necessary modules.
 sudo apt-get install apache2
 # Create symlinsk to load proper modules.
-sudo ln -s $MOD_AVA/proxy_balancer.conf $MOD_ENA/proxy_balancer.conf 
-sudo ln -s $MOD_AVA/proxy_balancer.load $MOD_ENA/proxy_balancer.load 
 sudo ln -s $MOD_AVA/proxy.conf $MOD_ENA/proxy.conf
 sudo ln -s $MOD_AVA/proxy.load $MOD_ENA/proxy.load
-# Restart apache.
-sudo $RUN_SCRIPT restart
-
+# Schedule updater script to run once every 10 mins.
+# See http://en.wikipedia.org/wiki/Cron for details.
+CRON_TIME='0,10,20,30,40,50 * * * *';
+crontab -l | { cat; echo "$CRON_TIME $UPDATE_SCRIPT"; } | crontab -
+# Run update script.
+$UPDATE_SCRIPT
--- LoadBalancer/updater.pl
+++ LoadBalancer/updater.pl
@@ -1,40 +1,127 @@
 #!/usr/bin/perl
 
-# Updates the servers on the balancer with those that are active.
-# Should be run occasionally so we can route around damaged servers.
-
 use strict;
 use warnings;
-use LWP::Simple;
 use Net::Ping;
+use POSIX qw(strftime);
+use File::stat;
+use Digest::MD5 'md5';
 
-my $Ping = Net::Ping->new();
-my $BalancerURL = 'localhost/balancer-manager';
-my $BalancerPage = get($BalancerURL);
+my $ApachePath = "/etc/apache2";
+my $ConfigPath = "$ApachePath/mods-available/proxy.conf";
+my $ServerPath = "servers.txt";
+my $LogPath = "updater.log";
+my $ApacheHardRestartCmd = '/etc/init.d/apache2 graceful';
+my $ApacheGracefulRestartCmd = '/etc/init.d/apache2 restart';
 
-sub formQueryString(%)
+my $Log;
+open($Log, ">>", $LogPath) or die "Unable to open logfile.\n";
+
+my %Servers = ();
+
+sub logwrite($)
 {
-	my %Hash = shift;
-	my $Query = "$BalancerURL?";
-	foreach my $Key (keys(%Hash)) {
-		$Query .= "$Key=" . uri_encode($Hash{$Key}) . "&";
-	}
+	print $Log strftime('[%Y/%m/%d %H:%M:%S]', localtime) . ": $_[0]\n";
 }
 
-while (my $Line =~ m/<form method=\"GET\" action=\"\/balancer-manager\">(.*?)<\/form>/g) {
-	my $Form = $1;
-	my %Parameters = ();
-	while ($Form =~ m/<input type=hidden name="(\w+)" value="(.*?)">/g) {
-		$Parameters{$1} = $2;
-	}
-	my $Hostname = $Parameters{'w'};
-	# We ping the server
-	if ($Ping->($Hostname)) {
-		get($BalancerURL . "?b=" . uriescape($Cluster) . "&w=" . uri_escape($Host) . "&nonce=" . $
-	}
-	else {
-		
-	}
+sub logdie($)
+{
+	logwrite($_[0]);
+	logwrite("Aborting...");
+	exit(-1);
 }
 
+logwrite("Script not running as root, may fail.") unless $> == 0;
+logwrite("Updating server list...");
+# Read in list of servers.
+open(my $Input, $ServerPath) or logdie("Unable to open servers.txt.\n");
+while (my $Line = <$Input>) {
+	next if $Line =~ m/^\s*$/;
+	next if $Line =~ m/^\s*#/;
+	die unless $Line =~ m/^\s*(.*?)\:?(\d+)?\s*$/;
+	my $Host = $1;
+	my $Port = $2;
+	$Servers{$Host} = defined $Port ? $Port : 80;
+}
+close($Input);
+# Make sure that they're all online.
+my $Ping = Net::Ping->new();
+foreach my $Server (keys(%Servers)) {
+	next if $Ping->ping($Server);
+	delete $Servers{$Server};
+	logwrite("Server $Server is not responding to ping, not adding to list.");
+}
 $Ping->close();
+# Generate configuration file.
+my $ConfigContents = "<IfModule mod_proxy.c>
+# 'ProxyRequests On' line and the <Proxy *> block below.
+# WARNING: Be careful to restrict access inside the <Proxy *> block.
+# Open proxy servers are dangerous both to your network and to the
+# Internet at large.
+#
+# If you only want to use apache2 as a reverse proxy/gateway in
+# front of some web application server, you DON'T need
+# 'ProxyRequests On'.
+
+#ProxyRequests On
+#<Proxy *>
+#        AddDefaultCharset off
+#        Order deny,allow
+#        Deny from all
+#        #Allow from .example.com
+#</Proxy>
+
+# Enable/disable the handling of HTTP/1.1 \"Via:\" headers.
+# (\"Full\" adds the server version; \"Block\" removes all outgoing Via: headers)
+# Set to one of: Off | On | Full | Block
+#ProxyVia Off
+
+ProxyPass /proxy balancer://mycluster nofailover=On
+<Proxy balancer://mycluster>
+";
+
+foreach my $Server (keys(%Servers)) {
+	$ConfigContents .= "	BalancerMember http://$Server:$Servers{$Server}\n";
+}
+
+$ConfigContents .= "</Proxy>
+
+</IfModule>";
+# Check if we differ from config file.
+my $Differ = 0;
+if (stat($ConfigPath)->size == length($ConfigContents) ) {
+	# Size is the same, let's do an md5 digest of our config, and the new one.
+	my $ConfigCurrent = undef;
+	{
+		local $/;
+		open($Input, $ConfigPath) or logdie("Unable to open configuration file for reading.");
+		$ConfigCurrent = <$Input>;
+		close($Input);
+	}
+	$Differ = 1 if (md5($ConfigContents) ne md5($ConfigCurrent));
+}
+else {
+	$Differ = 1;
+}
+
+if ($Differ == 1) {	
+	logwrite("Generated and current files differ. Replacing.");
+	open(my $Output, ">", $ConfigPath) or logdie("Unable to open configuration file for writing.");
+	print $Output $ConfigContents;
+	close($Output);
+	logwrite("Configuration updated sucessfully.");
+	logwrite("Gracefully restarting apache.");
+	# Perform a graceful restart.
+	my $ApacheLog = `$ApacheGracefulRestartCmd`;
+	if ($? != 0) {
+		logwrite("Unable to gracefully restart apache: " . $ApacheLog);
+		logwrite("Attempting full restart.");
+		$ApacheLog = `$ApacheHardRestartCmd`;
+		logdie("Unable to hard restart apache!") if $? != 0; 
+	}
+	logwrite("Succesfully restarted apache.");
+}
+else {
+	logwrite("Config files identical. No rewrite.");
+}
+
