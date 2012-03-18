25dd686f0041d3d1f6fdc7f27ed2c59252c4698b
Filipe Martinho <freshfil@gmail.com>
2012-03-18 19:37:38 -0400

    made class extend FrontCommand

--- MasterServer/src/application/commands/DeleteUserCommand.java
+++ MasterServer/src/application/commands/DeleteUserCommand.java
@@ -1,5 +1,24 @@
 package application.commands;
 
-public class DeleteUserCommand {
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import exceptions.MapperException;
+import exceptions.ParameterException;
+import exceptions.UnrecognizedUserException;
+
+public class DeleteUserCommand extends FrontCommand {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response)
+			throws MapperException, ParameterException, IOException,
+			UnrecognizedUserException, SQLException, ServletException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
