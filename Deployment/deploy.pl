2d6a9a6484ec9f00c026f7acd3d2c99288a7f0be
Adam Harrison <adamdharrison@gmail.com>
2012-03-25 02:42:05 -0400

    Put things into archive, readme, etc...

--- Deployment/deploy.pl
+++ Deployment/deploy.pl
@@ -10,15 +10,17 @@ my $RootPassword = $ARGV[0];
 my $Target = $ARGV[1];
 my $FilePath = $ARGV[2];
 
-my $FileName = basename($FilePath);
+my($FileName, $Directories, $Suffic) = fileparse($FilePath);
+
+die "Must be a zip archive." unless $Suffix eq "zip";
+my $SetupFile = "setup.sh";
 
 my $SCPE = Net::SCP::Expect->new;
 $SCPE->login('root', $RootPassword);
 die "Unable to scp file." unless $SCPE->scp($FilePath, "$Target:~/$FileName");
 my $SSH = Net::SSH::Perl->new($Target);
 die "Unable to SSH login." unless $SSH->login('root', $RootPassword);
-
-my ($STDOUT, $STDERR, $EXIT) = $SSH->cmd("chmod +x ~/$FileName && ~/$FileName");
+my ($STDOUT, $STDERR, $EXIT) = $SSH->cmd("unzip ~/$FileName && ~/$SetupFile");
 
 print "Successful installation.\n" if $EXIT == 0;
 print "Error in installation, dumping logs: $STDOUT; $STDERR.\n" if $EXIT != 0;
