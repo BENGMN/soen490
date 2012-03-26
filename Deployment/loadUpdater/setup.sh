9b4168405d51c8ac9da09e254d82def56e519d62
Adam Harrison <adamdharrison@gmail.com>
2012-03-26 16:49:13 -0400

    Comitting setup files.

--- Deployment/loadUpdater/setup.sh
+++ Deployment/loadUpdater/setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Installs the balancer updater on a particular machine.
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root." 1>&2
+   exit 1
+fi
+
 # Shell script to install the balancer updater from a base ubuntu distribution.
 MAIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 UPDATE_SCRIPT=$MAIN_DIR/updater.pl
@@ -9,13 +16,16 @@ OPTIONS=$@
 # Base update.
 apt-get update
 # Install apache and necessary modules.
-apt-get install perl curl
+apt-get install perl curl --assume-yes
+# Install self configured cpan.
 curl -L http://cpanmin.us | perl - --self-upgrade
 perl -MCPAN -e 'install Math::Pari'
 perl -MCPAN -e 'install Net::SSH::Perl'
 perl -MCPAN -e 'install Digest::MD5';
 perl -MCPAN -e 'install Net::MySQL';
 perl -MCPAN -e 'install Net::Ping';
+# Add a new user to run the script (so we don't have to run as root everytime)
+# With no password so we don't get an interactive prompt.
 adduser $USERNAME --disabled-password --gecos "" 
 # Schedule updater script to run once every 10 mins.
 # See http://en.wikipedia.org/wiki/Cron for details.
--- Deployment/masterServer/setup.sh
+++ Deployment/masterServer/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Installs the master server on the specified server.
+
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root." 1>&2
    exit 1
@@ -10,9 +12,13 @@ TOMCAT_PATH=/var/lib/tomcat6/webapps
 TOMCAT_UTIL=/etc/init.d/tomcat6
 	
 apt-get update
-apt-get install apache2 tomcat6
+# Install tomcat and apache.
+apt-get install apache2 tomcat6 --assume-yes
+# Move the .jar archive into the tomcat folder.
 mv $FILE_NAME $TOMCAT_PATH/
+# Make sure that tomcat can access the files we just moved in.
 chown tomcat6 -R $TOMCAT_PATH
+# Restart tomcat.
 $TOMCAT_UTIL restart
 
 exit 0
