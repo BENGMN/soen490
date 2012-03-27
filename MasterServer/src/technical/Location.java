cdfdb5b016c6a18fd2c6e18977e462fc382436af
Sotirios Delimanolis <sotodel_89@hotmail.com>
2012-03-27 18:45:24 -0400

    deletes

--- MasterServer/src/technical/Location.java
+++
@@ -1,8 +0,0 @@
-package technical;
-
-public interface Location {
-	public double getLongitude();
-	public void setLongitude(double longitude);
-	public double getLatitude();
-	public void setLatitude(double latitude);
-}
--- MasterServer/src/technical/UnrecognizedUserException.java
+++
@@ -1,6 +0,0 @@
-package technical;
-
-public class UnrecognizedUserException extends Exception {
-	private static final long serialVersionUID = -6387329253758991087L;
-
-}
